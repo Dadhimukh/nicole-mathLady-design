dec335abbf34402ed6a3fc8147f16545fd942c09
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-05-14 23:11:49 +0530

    Updated

---
+++ src/components/HeroHeaderSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DropdownHeaderNavigation from './DropdownHeaderNavigation'
+import Section from './Section'
+const HeroHeaderSection = () => {
+  return (
+    <div>
+      <DropdownHeaderNavigation />
+      <Section />
+    </div>
+  )
+}
+
+export default HeroHeaderSection
