c744df2e4c3666cb613f32ba288fc59e609b2d94
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-05-14 23:11:20 +0530

    Updated

---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Content from './Content'
+import NavigationAction from './NavigationAction'
+const Container = () => {
+  return (
+    <div>
+      <Content />
+      <NavigationAction />
+    </div>
+  )
+}
+
+export default Container
