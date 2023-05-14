b49547feaf075b679b1f5514e3c8ba2648c94dbc
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-05-14 23:11:57 +0530

    Updated

---
+++ src/components/NavigationAction.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavigationAction = () => {
+  return (
+    <>
+      <button type="button" class="btn btn-link">
+        Link
+      </button>
+      <button type="button" class="btn btn-success">
+        Success
+      </button>
+    </>
+  );
+};
+
+export default NavigationAction;
